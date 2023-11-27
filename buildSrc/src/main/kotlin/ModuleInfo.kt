68b52f71cc6f2a5de49013384551baf1a73520f5
maksonic <maksonic.official@yandex.ru>
2023-11-27 20:03:10 +0300

    000-000-002 Added buildSrc with toml version catalog.

---
+++ buildSrc/src/main/kotlin/ModuleInfo.kt
@@ -0,0 +1,11 @@
+/**
+ * @Author maksonic on 27.09.2023
+ */
+sealed class AbstractModule(val path: String, val namespace: String)
+
+object ModuleInfo {
+    object App : AbstractModule(
+        path = ":app",
+        namespace = "ru.maksonic.easypayments"
+    )
+}
