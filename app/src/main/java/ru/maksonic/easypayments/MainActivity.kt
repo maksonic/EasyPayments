0162a591602f0cef6c3cd0dee0a3903b689013e3
maksonic <maksonic.official@yandex.ru>
2023-11-27 19:48:09 +0300

    000-000-001 Initial.

---
+++ app/src/main/java/ru/maksonic/easypayments/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.maksonic.easypayments
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.maksonic.easypayments.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
